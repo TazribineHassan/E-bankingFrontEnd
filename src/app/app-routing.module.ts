1635c0d12a07737d106ff908ef41282652e077f3
Fatima-ZahraaJadour <fatimazahraajadour@gmail.com>
2021-06-06 03:14:26 +0100

    dashboard and clients table 2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { ClientComponent } from './client/client.component';
 import { LoginComponent } from './login/login.component';
 import {DashbordComponent} from "./agent/dashbord/dashbord.component";
 import {ClientsComponent} from "./agent/clients/clients.component";
+import {AjoutClientsComponent} from "./agent/ajout-clients/ajout-clients.component";
+import {VerserComponent} from "./agent/verser/verser.component";
+import {TransactionsComponent} from "./agent/transactions/transactions.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'client/home', component: ClientComponent },
   { path: 'agent', component: AgentComponent,
   children:[{path:'',component:DashbordComponent},
-    {path:'clients',component:ClientsComponent}
+    {path:'clients',component:ClientsComponent},
+    {path:'transactions',component:TransactionsComponent},
+    {path:'add-client',component:AjoutClientsComponent},
+    {path:'verse',component:VerserComponent}
   ]},
   { path: '', redirectTo: '/login', pathMatch:'full' }
 ];
