9a3526bc5535da9d1fcaeb7edae153f57e6a41d9
Fatima-ZahraaJadour <fatimazahraajadour@gmail.com>
2021-06-06 04:27:38 +0100

    dashboard and clients and transactions table 3nd commit

---
+++ src/app/agent/ajout-clients/ajout-clients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-clients',
+  templateUrl: './ajout-clients.component.html',
+  styleUrls: ['./ajout-clients.component.css']
+})
+export class AjoutClientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
