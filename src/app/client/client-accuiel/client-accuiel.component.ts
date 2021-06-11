17839f83b92dde62fd98541c433914eec5c7f32a
mohamedmatrouh <mohamed.matrouh16@gmail.com>
2021-06-11 18:25:37 +0100

    added payement to client

--- src/app/client/client-accuiel/client-accuiel.component.ts
+++ src/app/client/client-accuiel/client-accuiel.component.ts
@@ -11,11 +11,21 @@ import { UserService } from 'src/app/services/user.service';
 export class ClientAccuielComponent implements OnInit {
 
   public client: User = new User();
+  public num_factures: number = 0;
+  public num_virement: number = 0;
+  public num_versement: number = 0;
   
   constructor(private authenticationService: AuthenticationService, private userService: UserService) { }
 
   ngOnInit(): void {
     this.client = this.authenticationService.getUserFromLocalCache();
+    this.client.transactions.forEach(element => {
+
+      if(element.type_transaction === "payement") this.num_factures++;
+      if(element.type_transaction === "virement") this.num_virement++;
+      if(element.type_transaction === "versement") this.num_versement++;
+      
+    });
   }
 
 }
